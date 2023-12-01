883041f30711fc8e3cf2459e37adcd64758f9d21
Jamil <jamiljoocontact@gmail.com>
2023-12-01 13:53:34 +0100

    fix cors error

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/emotion": {"origins": "http://localhost:3000"}}, supports_credentials=True)
 
 from app import routes
